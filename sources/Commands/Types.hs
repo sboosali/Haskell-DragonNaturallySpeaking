8d39875dae37ca9e0bb0cc2137f4c364c01cacfc
Sam Boosalis <samboosalis@gmail.com>
2014-10-07 00:58:54 -0400

    Haddock
    "<...>" links can refer to local files
    
    "-- |" comments are haddock documentation

--- sources/Commands/Types.hs
+++ sources/Commands/Types.hs
@@ -4,36 +4,38 @@ import Commands.Etc
 import Data.Default
 
 
-
+-- | the core type sent to @'press'@
 data KeyPress = KeyPress Key [Modifier]
  deriving (Show)
 
+-- | modifier keys can be "held"
 data Modifier = Command | Control | Shift | Option | Function
  deriving (Show)
 
 -- | a @Char@ can be any Unicode character
 data Key = Key Char | Fun Integer | Mod Modifier
 
- | Escape -- ^ escape is pressed, not held. thus it's not a @Modifier@
+ | Escape -- ^ escape is "pressed", not "held". thus it's not a @Modifier@. (this is why you can't hold @esc-b@ in the terminal?)
 
- | Delete
- | ForwardDelete
+ | Delete        -- ^ deletes characters (isn't a character)
+ | ForwardDelete -- ^ deletes characters (isn't a character)
 
- | LeftArrow
- | RightArrow
- | DownArrow
- | UpArrow
+ | LeftArrow  -- ^ navigates characters (isn't a character)
+ | RightArrow -- ^ navigates characters (isn't a character)
+ | DownArrow  -- ^ navigates characters (isn't a character)
+ | UpArrow    -- ^ navigates characters (isn't a character)
 
  deriving (Show)
 
 
-
+-- | the core type sent to @'click'@
 data MouseClick = MouseClick MouseButton [Modifier] Positive
  deriving (Show)
 
 data MouseButton = LeftButton | MiddleButton | RightButton
  deriving (Show)
 
+-- | refinement
 newtype Positive = Positive_ Integer
  deriving (Show)
 
@@ -44,3 +46,4 @@ positive = smart def Positive_ (>= 1)
 
 data Context = Application String
  deriving (Show)
+
