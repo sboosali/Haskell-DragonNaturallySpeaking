a57eb5a6778720ac2aceae4f508d5557b1cc7320
Sam Boosalis <samboosalis@gmail.com>
2014-11-22 19:35:54 -0800

    .

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes #-}
 module Main where
 import Commands.Types
 import Commands.Munging
@@ -13,6 +13,7 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 import Test.HUnit
 
+import Control.Applicative
 import Data.BitVector
 import Data.Char
 
@@ -22,10 +23,9 @@ prop_hexadecimal_sizeInBits_is_multipleOfFour digits = all isHexDigit digits ==>
 
 prop_toConstructor_satisfies_isConstructor = maybe True isConstructor . toConstructor
 
-
 case_readsBitVector = ("0x00080000" :: BitVector) @?= ("0b00000000000010000000000000000000" :: BitVector)
 
 case_fromNSApplicationPath = fromNSApplicationPath "/Applications/Google Chrome.app" @?= GoogleChrome
 
+main = defaultMain [$(testGroupGenerator)]
 
-main = $(defaultMainGenerator)
