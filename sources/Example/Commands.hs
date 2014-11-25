a1ad06cc0788c9b4179e5dc98d8960af3b47477e
Sam Boosalis <samboosalis@gmail.com>
2014-11-24 19:25:53 -0800

    .

--- sources/Example/Commands.hs
+++ sources/Example/Commands.hs
@@ -25,21 +25,19 @@ instance Parse Words where
  parse context = Words <$> anyWord `manyUntil` context
 
 instance Parse Number where
- parse context = (Number . read) <$> (spaced $ Parsec.many1 Parsec.digit)
+ parse _ = (Number . read) <$> (spaced $ Parsec.many1 Parsec.digit)
 
 
 grammarTight = pProduction `parsing` [qq| data Command
 ReplaceWith  replace Phrase with Phrase
-Click        Times Button click
 Undo         undo |]
+
 
 grammarLoose = pGrammar `parsing` [qq| 
 
 data Command
 ReplaceWith  replace Phrase with Phrase
-Click        Times Button click
 Undo         undo
-
 
 data Stub
 Stub stub
@@ -49,16 +47,30 @@ Stub stub
 
 
 [rule| data Command
-ReplaceWith  replace Phrase with Phrase
-Click        Times Button click
-Undo         undo |]
+ReplaceWith    replace Phrase with Phrase
+Click          Times Button click
+TypeSignature  has type Phrase
+Undo           undo |]
 
 parseCommand :: String -> Either ParseError Command
 parseCommand = parsing (parse def)
 
 
+-- | tests:
+--
+-- * arbitrary whitespace
+-- * multiple productions
+--
+-- and:
+-- * adjacent non-terminals
+-- * adjacent terminals
+-- * suffix operator and prefix operator
+-- * single word
+-- 
 -- $ cabal build && cabal exec runhaskell sources/Example/Commands.hs
+--
 -- $ cabal exec -- ghc  -outputdir ignore/ignore  -ddump-splices  sources/Example/Commands.hs
+--
 main :: IO ()
 main = do
 
@@ -69,9 +81,11 @@ main = do
  putStrLn ""
  print $ ReplaceWith (Words ["this", "and", "that"]) (Words ["that", "and", "this"])
  print $ Click (Number 1) (Number 2)
+ print $ TypeSignature (Words ["maybe", "a"])
  print $ Undo
 
  putStrLn ""
  print $ parseCommand "replace this and that with that and this"
  print $ parseCommand "1 2 click"
+ print $ parseCommand "has type maybe a"
  print $ parseCommand "undo"
