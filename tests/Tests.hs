5309f4176aaafc76da53bc189bc22f0cc485cac1
Sam Boosalis <samboosalis@gmail.com>
2014-11-29 20:11:24 -0800

    refactor: Haskell already has hexadecimal literals

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -2,8 +2,6 @@
 module Main where
 import Commands.Types
 import Commands.Munging
-import Commands.Instances()
-import Commands.Bits
 import Commands.OSX.Bridge
 
 import Test.Framework (defaultMain, testGroup)
@@ -18,12 +16,7 @@ import Data.BitVector
 import Data.Char
 
 
-prop_hexadecimal_sizeInBits_is_multipleOfFour digits = all isHexDigit digits ==>
- size (read $ "0x" ++ digits) `mod` 4 == 0
-
 prop_toConstructor_satisfies_isConstructor = maybe True isConstructor . toConstructor
-
-case_readsBitVector = ("0x00080000" :: BitVector) @?= ("0b00000000000010000000000000000000" :: BitVector)
 
 case_fromNSApplicationPath = fromNSApplicationPath "/Applications/Google Chrome.app" @?= GoogleChrome
 
